c98746c69de0a6aab951ac87f6e24f2eca56b33a
Juanjoxznp <juanjoxznp@gmail.com>
2023-09-21 19:30:24 -0500

    Segundo commit feature/js

--- game.js
+++ game.js
@@ -32,3 +32,113 @@ while(counterRound<2){
         case "1": {
 
         switch (opcionTurnoPlayerTwo){
+
+            case "1": {
+
+                alert(`Jugador 1 ha sacado ${turnPlayerOne}`);
+                alert(`Jugador 2 ha sacado ${turnPlayerTwo}`);
+    
+                counterPlayerOne+=turnPlayerOne
+                counterPlayerTwo+=turnPlayerTwo
+                counterRound++
+                alert(`El acumulado del jugador 1 es: ${counterPlayerOne}`)  ;
+                alert(`El acumulado del jugador 2 es: ${counterPlayerTwo}`)  ;
+    
+                alert(`Ronda ${counterRound} finalizada`);
+    
+                if(counterPlayerOne>21 && counterPlayerTwo>21){
+    
+                    alert(`Ambos han perdido, han sobrepasado el puntaje de 21`)
+    
+                } else if(counterPlayerOne>21 && counterPlayerTwo <=21){
+    
+                    alert(`El jugador 2 ha ganado, ya que el jugador 1 superó el puntaje de 21`)
+    
+                } else if(counterPlayerOne<=21 && counterPlayerTwo >21){
+    
+                    alert(`El jugador 1 ha ganado, ya que el jugador 2 superó el puntaje de 21`)
+    
+                } else if(counterPlayerOne>counterPlayerTwo){
+    
+                    alert(`El jugador 1 ha ganado`)
+    
+                } else if(counterPlayerOne<counterPlayerTwo){
+    
+                    alert(`El jugador 2 ha ganado`)
+    
+                } else if(counterPlayerOne===counterPlayerTwo){
+    
+                    alert(`HAY UN EMPATE`)
+    
+                } 
+    
+                 break
+                 
+            }
+            case "2": {
+                    
+                alert(`Jugador 1 ha sacado ${turnPlayerOne}`);
+                alert(`Jugador 2 se ha plantado`);
+    
+                counterPlayerOne+=turnPlayerOne
+                counterRound++
+    
+                alert(`El acumulado del jugador 1 es: ${counterPlayerOne}`)  ;
+                alert(`El acumulado del jugador 2 es: ${counterPlayerTwo}`)  ;
+    
+                alert(`Ronda ${counterRound} finalizada`) 
+    
+                if(counterPlayerOne>21){
+    
+                    alert(`El jugador 2 ha ganado, ya que el jugador 1 sobrepaso el puntaje de 21`)
+    
+                }else if(counterPlayerOne>counterPlayerTwo){
+    
+                    alert(`El jugador 1 ha ganado`)
+    
+                }else if(counterPlayerOne<counterPlayerTwo){
+    
+                    alert(`El jugador 2 ha ganado`)
+    
+                }else if(counterPlayerOne===counterPlayerTwo){
+    
+                    alert(`HAY UN EMPATE`)
+                } 
+    
+                break}
+            
+    
+        }  break
+    }           case "2": {
+    
+        switch (opcionTurnoPlayerTwo){
+    
+               
+            case "1": {
+    
+                alert(`Jugador 1 se ha plantado`);
+                alert(`Jugador 2 ha sacado ${turnPlayerTwo}`);
+    
+                counterPlayerTwo+=turnPlayerTwo
+                counterRound++
+                alert(`El acumulado del jugador 1 es: ${counterPlayerOne}`)  ;
+                alert(`El acumulado del jugador 2 es: ${counterPlayerTwo}`)  ;
+    
+                alert(`Ronda ${counterRound} finalizada`)
+    
+                if(counterPlayerTwo>21){
+    
+                    alert(`El jugador 1 ha ganado, ya que el jugador 2 sobrepaso el puntaje de 21`)
+    
+                }else if(counterPlayerOne>counterPlayerTwo){
+    
+                    alert(`El jugador 1 ha ganado`)
+    
+                }else if(counterPlayerOne<counterPlayerTwo){
+    
+                    alert(`El jugador 2 ha ganado`)
+    
+                }else if(counterPlayerOne===counterPlayerTwo){
+    
+                    alert(`HAY UN EMPATE`)
+                } 
